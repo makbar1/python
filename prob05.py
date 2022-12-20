eddb406feb08069c03d8b3713486afce48595f9a
M A <akbar00@yahoo.com>
2022-12-20 16:47:42 -0600

    12/20/2022

--- prob05.py
+++ prob05.py
@@ -1,9 +1,8 @@
 import math
 
-
-for n in range(1, 4):
-    for d in range(1, 4):
-        if n % d == 0:
-            print(n,d)
+for n in range(1, 50000000000):
+    if (n % 2 == 0 and n % 3 == 0 and n % 4 == 0 and n % 5 == 0 and n % 6 == 0 and n % 7 == 0 and n % 8 and n % 9 == 0 and n % 10 == 0):
+        if (n % 11 == 0 and n % 12 == 0 and n % 13 and n % 15 == 0 and n % 16 ==0 and n % 17 == 0 and n % 18 ==0 and n % 19 == 0 and n % 20 ==0):
+            print(n," is Divisible by 1..20")
 		
 	
