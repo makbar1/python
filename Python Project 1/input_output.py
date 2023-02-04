7e67afde5c6ff643a47526fdeae5bfb7dfe64a76
M A <akbar00@yahoo.com>
2023-02-04 13:54:15 -0500

    2/4/2023

---
+++ Python Project 1/input_output.py
@@ -0,0 +1,6 @@
+#x = input("Give me a number")
+#print(x)
+
+x, y = input("Give me a number :").split()
+print(x)
+print(y)
---
+++ Python Project 1/lots_of_conditions_if.py
@@ -0,0 +1,12 @@
+a= 11
+b = 3
+c =50
+
+conditions = [
+    a > 10,
+    b < 4,
+    c > 30
+]
+
+if all(conditions):
+    print('Wow')
