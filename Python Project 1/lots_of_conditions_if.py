8afca3c77b9b3e2cb5ceeaa0b2a6a764d29ca4e2
M A <akbar00@yahoo.com>
2023-02-05 14:21:48 -0500

    2/5/2023

--- Python Project 1/lots_of_conditions_if.py
+++ Python Project 1/lots_of_conditions_if.py
@@ -11,6 +11,8 @@ conditions = [
 # If all conditions are met i.e AND for all
 if all(conditions):
     print('Wow')
+#
+
 # If any of the conditions are met i.e OR
 if any(conditions):
     print("Super Wow")
